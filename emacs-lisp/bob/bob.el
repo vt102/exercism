6acc9166d45e252ce64f9f8e9d399e429e448881
Andy Cowell <andy@cowell.org>
2024-09-11 13:51:29 +0000

    Finished bob

--- emacs-lisp/bob/bob.el
+++ emacs-lisp/bob/bob.el
@@ -4,10 +4,21 @@
 
 ;;; Code:
 
-
 (defun response-for (phrase)
- (error "Delete this S-Expression and write your own implementation"))
-
+  "Documentation string"
+  (let* ((case-fold-search nil)
+         (phrase (replace-regexp-in-string "[[:blank:]]*$" "" phrase))
+         (phrase (replace-regexp-in-string "[\n\r]" "" phrase))
+         (question-flag (string-match  "\?$" phrase))
+         (caps-flag (and (string-match "[A-Z]" phrase)
+                         (string-match "^[^a-z]*$" phrase)))
+         (ws-flag (string-match "^$" phrase)))
+    (cond
+     ((and question-flag caps-flag) "Calm down, I know what I'm doing!")
+     (question-flag "Sure.")
+     (caps-flag "Whoa, chill out!")
+     (ws-flag "Fine. Be that way!")
+     (t "Whatever."))))
 
 (provide 'bob)
 ;;; bob.el ends here
